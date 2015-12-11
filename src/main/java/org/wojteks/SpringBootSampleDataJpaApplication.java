4be0ca3476085bd6f82ba70783590964364abe33
WojtekSasiela <wojtek.sasiela@gmail.com>
2015-12-11 10:47:46 +0100

    Initial commit

---
+++ src/main/java/org/wojteks/SpringBootSampleDataJpaApplication.java
@@ -0,0 +1,12 @@
+package org.wojteks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSampleDataJpaApplication.class, args);
+    }
+}
